9eb10a5ea693e9aa6305323f065d3901af40f5e0
alfredopizana <pizana.alfredo@gmail.com>
2021-08-26 01:16:27 -0500

    Created first block of content with nunjucks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 const gulp = require("gulp");
-
+const nunjucksRender = require("gulp-nunjucks-render")
 gulp.task("default", async function(){
-    return gulp.src("src/*").pipe(gulp.dest("dist"))
+    return gulp.src("src/pages/*")
+    .pipe(
+        nunjucksRender({
+            path: ["src/templates"]
+        })
+    )
+    .pipe(gulp.dest("dist"))
 })
